0b765e34abbe9cd2c7663074e045829b271ec57b
Siddhartha Reddy <siddhusandy149@gmail.com>
2023-10-23 19:48:26 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,6 +1,60 @@
 import re
 import pandas as pd
 
+def preprocessiosy12(data):
+    pattern = r'\[\d{2}/\d{2}/\d{2}, \d{1,2}:\d{2}:\d{2}\s?[APap][Mm]\]'
+    
+    # Extract timestamps using the pattern
+    dates = re.findall(pattern, data)
+    
+    # Extract messages by splitting the data using the pattern
+    messages = re.split(pattern, data)[1:]
+    
+    # Remove the non-breaking space from the timestamps
+    dates = [date.replace('\u200B', ' ') for date in dates]
+    
+    # Convert timestamps to datetime objects
+    date_objs = pd.to_datetime(dates, format='[%d/%m/%y, %I:%M:%S %p]')
+    
+    # Create a DataFrame
+    df = pd.DataFrame({'date': date_objs, 'user_message': messages})
+    
+    users = []
+    messages = []
+    
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+    
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+    df['day_name'] = df['date'].dt.day_name()
+    df['only_date'] = df['date'].dt.date
+    df['YEAR'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['MONTH'] = df['date'].dt.strftime('%B')
+    df['DAY'] = df['date'].dt.day
+    df['HOUR'] = df['date'].dt.hour
+    df['MINUTE'] = df['date'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'HOUR']]['HOUR']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str(hour) + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+    df['period'] = period
+
+    return df
+
 
 def preprocessy12(data):
     pattern = r'\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s[APap][Mm]\s-\s'
